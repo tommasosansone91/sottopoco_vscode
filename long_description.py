fac10f78db0dee5259b842603c4a8e406bdb59fd
Tommaso Sansone <tommaso.sansone91@virgilio.it>
2023-08-27 20:42:45 +0200

    update link to the cover

--- long_description.py
+++ long_description.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION = \
 """
 
 <div style="text-align:center">
-	<img src="https://raw.githubusercontent.com/tommasosansone91/canederli/main/images/sottopoco_vscode1.png" style="width:60%;" align="middle" alt="cover of Canaderli python package" >
+	<img src="https://raw.githubusercontent.com/tommasosansone91/sottopoco_vscode/main/images/sottopoco_vscode1.png" style="width:60%;" align="middle" alt="cover of sottopoco_vscode python package" >
 </div>
 
 <br>
